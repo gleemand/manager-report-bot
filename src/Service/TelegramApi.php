b93fb93532deeb8ea5563736e5e19a6c4bdd7841
Almaz Bazarov <gleemand@gmail.com>
2023-06-29 17:41:10 +0200

    Added exception catching

--- src/Service/TelegramApi.php
+++ src/Service/TelegramApi.php
@@ -2,20 +2,27 @@
 
 namespace App\Service;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use TelegramBot\Api\BotApi;
 
 class TelegramApi
 {
     private BotApi $botApi;
+    private LoggerInterface $logger;
 
-    public function __construct(ContainerInterface $container)
+    public function __construct(ContainerInterface $container, LoggerInterface $logger)
     {
         $this->botApi = new BotApi($container->getParameter('tg_token'));
+        $this->logger = $logger;
     }
 
     public function sendMessage(string $chat, string $message)
     {
-        $this->botApi->sendMessage($chat, $message, 'Markdown');
+        try {
+            $this->botApi->sendMessage($chat, $message, 'Markdown');
+        } catch (\Exception $exception) {
+            $this->logger->error($exception->getMessage());
+        }
     }
 }
